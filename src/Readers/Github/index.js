436ecc950e924fdaf0b77c541ef26fca1b456a59
Harminder Virk <virk.officials@gmail.com>
2016-09-18 21:53:56 +0530

    refactor(readers): support for extension filter and gh recursive read
    now can read docs from Github recursively and filter docs based of extension

--- src/Readers/Github/index.js
+++ src/Readers/Github/index.js
@@ -9,50 +9,79 @@
  * file that was distributed with this source code.
 */
 
-const octonode = require('octonode')
-const Q = require('q')
-const _ = require('lodash')
+const path = require('path')
+const shelljs = require('shelljs')
+const FsReader = require('../FS')
 
 class GithubReader {
 
-  constructor (repo, branch, fromPath) {
-    this.branch = branch
-    this.fromPath = fromPath || ''
-    this.repo = octonode.client(process.env.GH_TOKEN).repo(repo)
+  constructor (repo, branch, fromPath, docExtensions) {
+    this.branch = branch || 'master'
+    docExtensions = docExtensions || ['.md', '.adoc']
+    /**
+     * Temporary cloning the repo for reading content
+     * recursively.
+     */
+    this.tmpPath = path.join(__dirname, '../../../tmp')
+    this.fsReader = new FsReader(path.join(this.tmpPath, fromPath), docExtensions)
+    this.cloneUrl = process.env.GH_TOKEN ? `https://${process.env.GH_TOKEN}@github.com/${repo}` : `https://github.com/${repo}`
   }
 
-  _readRemoteContents (fromPath, callback) {
+  /**
+   * Clones repo to a temporary path
+   *
+   * @return  {Promise}
+   *
+   * @private
+   */
+  _cloneRepo () {
     return new Promise((resolve, reject) => {
-      this.repo.contents(fromPath, this.branch, (error, contents) => {
-        if (error) { return reject(error) }
-        resolve(callback(contents))
+      shelljs.exec(`git clone -b ${this.branch} --single-branch ${this.cloneUrl} ${this.tmpPath}`, (error) => {
+        if (error) {
+          return reject(error)
+        }
+        resolve()
       })
     })
   }
 
-  _getDocsTree () {
-    return this._readRemoteContents(this.fromPath, (contents) => {
-      return _(contents)
-        .filter((item) => item.download_url && item.path.endsWith('.md'))
-        .map((item) => item.path)
-        .value()
+  /**
+   * Removes the repo from the temporary path using
+   * rm -rf command.
+   *
+   * @return  {Promise}
+   *
+   * @private
+   */
+  _removeClonedRepo () {
+    return new Promise((resolve, reject) => {
+      shelljs.exec(`rm -rf ${this.tmpPath}`, (error) => {
+        if (error) {
+          return reject(error)
+        }
+        resolve()
+      })
     })
   }
 
-  _getDocContent (fromPath) {
-    return this._readRemoteContents(fromPath, (contents) => {
-      return {path: fromPath, contents: new Buffer(contents.content, 'base64').toString('utf-8')}
-    })
-  }
-
+  /**
+   * Read docs by cloning a repo#branch to a temporary path and
+   * then makes use of FsReader to recursively read the
+   * contents and removes the cloned repo.
+   *
+   * @return {Array}
+   */
   getDocs () {
+    let _contents = []
     return new Promise((resolve, reject) => {
       this
-        ._getDocsTree()
-        .then((files) => {
-          return Q.all(_.map(files, (file) => this._getDocContent(file)))
+        ._cloneRepo()
+        .then(() => this.fsReader.getDocs())
+        .then((contents) => {
+          _contents = contents
+          return this._removeClonedRepo()
         })
-        .then(resolve)
+        .then(() => resolve(_contents))
         .catch(reject)
     })
   }
